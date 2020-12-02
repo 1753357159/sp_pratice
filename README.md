e58618d091c8b3319d1e3912dc64d974a266bdff
garylin219 <garylin219@gmail.com>
2020-12-02 23:33:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Getting started
+
+Run in this directory:
+```
+python3 regex.py [string you want to certification]
+```
+it will show what violation that your string have.
+
+#Bouns questions
+
+Yes, we had done.
---
+++ regex.py
@@ -0,0 +1,53 @@
+import sys
+import re
+
+str1 = sys.argv[1]
+printFail = False
+errorMsg = []
+
+regex_string = re.search('[a-zA-z]+', str1)
+if regex_string is None:
+    printFail = True
+    errorMsg.append('缺少英文')
+
+regex_number = re.search('[0-9]+', str1)
+if regex_number is None:
+    printFail = True
+    errorMsg.append('缺少數字')
+
+regex_symbol = re.search('[^[a-zA-Z0-9\s]+', str1)
+if regex_symbol is None:
+    printFail = True
+    errorMsg.append('缺少符號')
+
+regex_lenMin = re.search('.{8,16}', str1)
+if (regex_lenMin is None):
+    printFail = True
+    errorMsg.append('長度須大於8')
+
+regex_lenMax = re.search('.{17,}', str1)
+if (regex_lenMax is not None):
+    printFail = True
+    errorMsg.append('長度須小於17')
+
+
+
+regex_upper = re.search('[A-Z]+', str1)
+if (regex_upper is None):
+    printFail = True
+    errorMsg.append('缺少英文大寫')
+
+str1 = ''.join(re.findall('[a-zA-Z0-9]*', str1))
+for i in range(len(str1)):
+    if i>0:
+        if ord(str1[i])==(ord(str1[i-1])+1):
+            printFail = True
+            errorMsg.append('字母與數字不可以連序')
+            break
+
+
+if (printFail):
+    print('輸入字串不符合以下規則：')
+    print('、'.join(errorMsg))
+else :
+    print('success')
